330c072e465760b410e296b8e23f49d2f6b90e12
akiyuki.kamiura <akiyuki.kamiura@bizreach.co.jp>
2020-04-22 11:04:11 +0900

    Step01 - test_variable_instance_variable_default_Integer()

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -108,7 +108,8 @@ public class Step01VariableTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_Integer() {
         Integer sea = instanceHangar;
-        log(sea); // your answer? =>
+        log(sea); // your answer? => null or 0
+        // Integer の初期化は null で行われる
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
