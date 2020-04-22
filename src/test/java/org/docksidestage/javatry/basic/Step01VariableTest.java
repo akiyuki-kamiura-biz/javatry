d7546060904e53f0fec95c70aab7bedea3d0cc07
akiyuki.kamiura <akiyuki.kamiura@bizreach.co.jp>
2020-04-22 09:50:54 +0900

    Step01VariableTest:test_variable_initial()

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -47,7 +47,7 @@ public class Step01VariableTest extends PlainTestCase {
         String piari = null;
         String dstore = "mai";
         sea = sea + land + piari + ":" + dstore;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => mystic8null:mai
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
