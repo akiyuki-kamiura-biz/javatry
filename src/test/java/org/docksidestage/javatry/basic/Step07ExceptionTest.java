008aa4785f996dc89999639e89e6fee785ae2c4f
akiyuki.kamiura <akiyuki.kamiura@bizreach.co.jp>
2020-04-23 16:22:45 +0900

    Step07 - test_exception_hierarchy_Runtime_instanceof_Error()

--- src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
@@ -67,7 +67,7 @@ public class Step07ExceptionTest extends PlainTestCase {
     public void test_exception_hierarchy_Runtime_instanceof_Error() {
         Object exp = new IllegalStateException("mystic");
         boolean sea = exp instanceof Error;
-        log(sea); // your answer? =>
+        log(sea); // your answer? => false
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
