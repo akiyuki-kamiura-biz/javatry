e6c4aa680e58c16fbd8d58b51e4638ec8c268c06
akiyuki.kamiura <akiyuki.kamiura@bizreach.co.jp>
2020-04-24 15:26:38 +0900

    Debug/Step23 - test_wordPoolSorter()

--- src/test/java/org/docksidestage/javatry/debug/Step23SorterTest.java
+++ src/test/java/org/docksidestage/javatry/debug/Step23SorterTest.java
@@ -53,7 +53,7 @@ public class Step23SorterTest extends PlainTestCase {
         // arrange
         Language language = new LanguagePool().getLanguage("Japanese");
         List<Word> expected =
-                Arrays.asList(new Word(language, "昴"), new Word(language, "柿"), new Word(language, "私"), new Word(language, "荼"));
+                Arrays.asList(new Word(language, "昂"), new Word(language, "柿"), new Word(language, "私"), new Word(language, "荼"));
 
         // act
         List<Word> result = new WordAssort().sort();
