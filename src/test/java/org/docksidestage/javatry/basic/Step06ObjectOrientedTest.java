25a03af8457bc015801312d0872c09baa770074a
akiyuki.kamiura <akiyuki.kamiura@bizreach.co.jp>
2020-05-20 10:57:01 +0900

    Step6 BarkingProcess について

--- src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
@@ -480,10 +480,14 @@ public class Step06ObjectOrientedTest extends PlainTestCase {
 
         // もう少しいい方法があれば教えていただきたいです。
 
-        // TODO kamiura なるほどですね、調べてありがとう。そうですね、ここの質問はちゃんとAnimalの子供達に対応しているか、どう対応するのか注目ポイントで、
+        // TODO done kamiura なるほどですね、調べてありがとう。そうですね、ここの質問はちゃんとAnimalの子供達に対応しているか、どう対応するのか注目ポイントで、
         // zombieを対応すること漏れずに対応したのはいいことです。が、zombieはAnimalを継承しているのにconstructorでsuper()をしないのはあまりよくありせん。
         // それを修正するのは、BarkProcessを初期化する処理を切り出してみてください。
         // これを自分の力を修正してみたら、他のいい方法揉みたいなら、自分の方もよかったので、参考してみてもいいかもしてません。winkichanwi 20200511
+        // TODO winkichanwi ありがとうございます。
+        //  winki さんの javatry を見させていただきましたが、
+        //  BarkProcess のコンストラクタに animal を渡すという考えは、自分は考えが及んでいませんでした。
+        //  そちらのやり方の方がシンプルで可読性が高く、拡張もしやすいと思いました。
         Dog dog = new Dog();
         BarkedSound barkedSound = dog.bark();
         log(barkedSound.getBarkWord());
